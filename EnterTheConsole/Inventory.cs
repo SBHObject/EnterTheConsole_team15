290784f3f82ebcc545a085734af68ba6ffbee214
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-09-27 15:49:39 +0900

    fix: annotation
    주석추가

--- EnterTheConsole/Inventory.cs
+++ EnterTheConsole/Inventory.cs
@@ -30,6 +30,7 @@
                 }
             }
 
+            //반복문에서 return을 못할경우, 인벤토리가 꽉 찬상태 -> 실패 반환
             Console.WriteLine("인벤토리가 꽉찼습니다.");
             Thread.Sleep(1000);
             return false;
@@ -38,14 +39,17 @@
         //인벤토리 아이템 제거 함수
         public bool RemoveItem(int indexNum)
         {
+            //인덱스를 매개변수로 받아서, 인덱스위치의 아이템 확인
             if (playerInven[indexNum] == null)
             {
                 Console.WriteLine("선택한 슬롯에 아이템이 없습니다.");
                 Thread.Sleep(1000);
+                //실패 반환
                 return false;
             }
             else
             {
+                //인덱스에 아이템이 있으면 삭제
                 playerInven[indexNum] = null;
                 return true;
             }
@@ -53,16 +57,20 @@
 
         public bool UseItem(int index)
         {
+            //아이템의 종류에 따라 달라짐
             switch(playerInven[index].ItemType)
             {
+                //무기일경우
                 case ItemType.Weapon:
+                    //장비 슬롯에 아이템이 없으면 장착
                     if (playerEquip[0] == null)
                     {
                         playerEquip[0] = playerInven[index];
-                        playerInven[index] = null;
+                        RemoveItem(index);
                     }
                     else
                     {
+                        //있으면 교체
                         IItem temp = playerEquip[0];
                         playerEquip[0] = playerInven[index];
                         playerInven[index] = temp;
@@ -70,11 +78,13 @@
                     Console.WriteLine($"{playerInven[index].ItemName}를 장착했습니다");
                     return true;
 
+                //방어구일경우
                 case ItemType.Armor:
+                    //무기와 같은 기능, 슬롯이 달라짐
                     if (playerEquip[1] == null)
                     {
                         playerEquip[1] = playerInven[index];
-                        playerInven[index] = null;
+                        RemoveItem(index);
                     }
                     else
                     {
@@ -85,10 +95,12 @@
                     Console.WriteLine($"{playerInven[index].ItemName}를 장착했습니다");
                     return true;
 
+                //소모품인경우
                 case ItemType.Consumable:
                     Console.WriteLine("소모품을 사용하였습니다.");
                     return true;
 
+                //그 외 입력이 들어올경우 아이템 사용 실패 -> 버그
                 default:
                     Console.WriteLine("아이템 사용에 실패하였습니다.");
                     return false;
