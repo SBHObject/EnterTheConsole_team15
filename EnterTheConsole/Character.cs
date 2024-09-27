3fce08ab5c871ca3eab6eb5375583c02d181623c
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-09-27 20:14:18 +0900

    fix: Additinal character class function
    캐릭터 클래스에 함수, 프로퍼티등 추가

--- EnterTheConsole/Character.cs
+++ EnterTheConsole/Character.cs
@@ -26,6 +26,13 @@
         //소지금
         public int PlayerGold { get; private set; }
 
+        //레벨관련 -> 경험치와 레벨
+        public int PlayerExp { get; private set; } = 0;
+        public int PlayerLevel { get; private set; } = 0;
+
+        //사망관련
+        public bool IsDead { get; private set; } = false;
+
         //생성자 - 기본값 초기화
         /// <summary>
         /// new Character 생성자 사용하지 말고 Program.player 사용해서 객체 가져올것
@@ -55,7 +62,7 @@
                 Console.WriteLine("캐릭터 이름을 입력해주세요");
                 string nameInput = Console.ReadLine();
                 Console.Clear();
-                Console.WriteLine("이 이름이 맞습니까?");
+                Console.WriteLine($"\"{nameInput}\" 이 이름이 맞습니까?");
                 Console.WriteLine($"1.예 2.아니오");
                 if(ut.Selecter(Console.ReadLine(), 2) == 1)
                 {
@@ -63,6 +70,43 @@
                     break;
                 }
             }
+        }
+
+        //플레이어가 데미지 받는 함수
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            //TODO 사망
+        }
+
+        //돈 얻기
+        public void GetGold(int amount)
+        {
+            PlayerGold += amount;
+        }
+
+        //돈 쓰기
+        public bool LoseGold(int amount)
+        {
+            //돈이 부족하면 false 반환, 돈을 차감하지 않음
+            if(amount > PlayerGold)
+            {
+                Console.WriteLine("소지금이 부족합니다.");
+                return false;
+            }
+            else
+            {
+                PlayerGold -= amount;
+                return true;
+            }
+        }
+
+        //체력 회복
+        public void GetHeal(int amount)
+        {
+            int temp = Health + amount;
+            //회복량이 체력 최대치를 넘으면, 최대체력으로
+            Health = (temp > maxHealth)? maxHealth : temp;
         }
     }
 }
