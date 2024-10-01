3bda5d67fd72be53e2d150a82ad00bb4bd76142f
hyobeen12 <a55974124@gmail.com>
2024-10-01 16:54:59 +0900

    fix : Status
    캐릭터 클래스에서 정보를 가져오게 변경했습니다.

--- EnterTheConsole/Status.cs
+++ EnterTheConsole/Status.cs
@@ -1,58 +1,21 @@
-﻿namespace EnterTheConsole
+﻿using Play_LHY;
+using System.Linq.Expressions;
+
+namespace EnterTheConsole
 {    
         internal class Status
         {
-            //이름
-            public string Name { get; private set; }
+        public string Job {  get; set; }
 
-            //직업
-            public string Job { get; private set; }
-
-            //최대 체력
-            private int maxHealth;
-            //현재 체력
-            public int Health { get; private set; }
-
-            //기본 공격력
-            private int attack;
-            //실제 공격력
-            public int Attack { get; private set; }
-
-            //기본 방어력
-            private int defence;
-            //실제 방어력
-            public int Defence { get; private set; }
-
-            //소지금
-            public int PlayerGold { get; private set; }
-
-            //레벨
-            public int PlayerLevel { get; private set; } = 0;
-
-            public Status(int level, string name, string job, int atk, int def, int hp, int gold)
+            public void DisplayCharacterInfo(Character player)
             {
-                PlayerLevel = level;
-                Name = name;
-                Job = job;
-                attack = atk;
-                defence = def;
-                Health = hp;
-                PlayerGold = gold;
-            }
-
-            public Status()
-            {
-            }
-
-            public void DisplayCharacterInfo()
-            {
-
-                Console.WriteLine($"LV. {PlayerLevel:D2}");
-                Console.WriteLine($"{Name} ( {Job} )");
-                Console.WriteLine(Attack == 0 ? $"공격력 : {attack}" : $"공격력 : {Attack} ");
-                Console.WriteLine(Defence == 0 ? $"방어력 : {defence}" : $"방어력 : {Defence}");
-                Console.WriteLine($"체 력 : {Health}");
-                Console.WriteLine($"Gold : {PlayerGold} G");
+                
+                Console.WriteLine($"LV. {player.PlayerLevel:D2}");
+                Console.WriteLine($"{player.Name} ( {Job} )");
+                Console.WriteLine($"공격력 : {player.Attack} ");
+                Console.WriteLine($"방어력 : {player.Defence}");
+                Console.WriteLine($"체 력 : {player.Health}");
+                Console.WriteLine($"Gold : {player.PlayerGold} G");
             }
         }
     
